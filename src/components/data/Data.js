020eccd6d5a5b1415d39d01ded97a8f98e39ce21
Belay <155488654+Belasoftengg@users.noreply.github.com>
2024-05-30 12:08:20 +0300

    Update Data.js

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -311,14 +311,14 @@ export const price = [{
             ]
             export const footer = [{
                     title: "LAYOUTS",
-                    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+                    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Contact Page" }],
                 },
                 {
                     title: "ALL SECTIONS",
-                    text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+                    text: [{ list: "Headers" }, { list: "Features" },  { list: "Testimonials" }, { list: "Footers" }],
                 },
                 {
                     title: "COMPANY",
-                    text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+                    text: [{ list: "About" }, { list: "Blog" },  { list: "Affiliate" }, { list: "Login" }],
                 },
             ]
